0e9d0238ba018c2a4225dc66f500d496f4b36bf0
DominusMortem <80106081+DominusMortem@users.noreply.github.com>
2021-04-04 18:45:28 +0300

    Update Telegram.hs
    Working on Handle

--- src/Telegram.hs
+++ src/Telegram.hs
@@ -19,10 +19,12 @@ import Data.Text.IO as TIO
 import qualified Data.ByteString.Lazy.Char8 as LBC
 import qualified Data.Map as M
 
-new :: IO Handle
-new = return $ Handle { initBot = response
-                      , updateBot = responseOffset
-                      , offset = 0}
+new :: Handle
+new = Handle { initBot = response
+             , updateBot = responseOffset
+             , offset = 0
+             , dictonary = M.empty
+             }
 
 response :: IO B.ByteString
 response = do
@@ -33,50 +35,54 @@ responseOffset :: Int -> IO B.ByteString
 responseOffset id = do 
     res <- httpLBS $ getUpdatesWithOffset id
     return (getResponseBody res)
-{--
-vib :: Int -> IO B.ByteString
-vib id = do
+
+vib :: Handle -> IO B.ByteString
+vib hand = do
+    let id = offset hand
     case id of
-        0 -> response
-        _ -> responseOffset id--}
+        0 -> initBot hand
+        _ -> (updateBot hand) id
           
-testIO :: Handle -> M.Map Int String -> IO ()
-testIO hand dict = do
-    res <- vib id
+testIO :: Handle -> IO ()
+testIO hand = do
+    res <- vib hand
     let m = decode res :: Maybe UpdateRes
     case m of
-        Nothing -> testIO id dict
+        Nothing -> testIO hand
         Just response -> do
             let rez = result response
             case rez of
-                [] -> testIO id dict
+                [] -> testIO hand
                 (x:xs) -> do
                   let up_id = update_id x
+                  let dict = dictonary hand
                   if (message x) == Nothing
                   then do
                     let cb = callback_query x
                     let d = fromJust $ data_query $ fromJust cb
                     let a = from_id_query $ fromJust cb
                     let dict' = (testMap' a d dict)
+                    let hand' = hand {dictonary = dict', offset = (up_id + 1)}
                     print dict'
-                    testIO (up_id + 1) dict'
+                    testIO hand'
                   else do
                     let mes = message x
                     let t = textT mes
                     let u = fromId mes
                     let dict' = (testMap u "1" dict)
+                    let hand' = hand {dictonary = dict', offset = (up_id + 1)}
                     print dict'
                     case t of
                       "/help" -> do
                         httpLBS $ sendMessage mes
-                        testIO (up_id + 1) dict'
+                        testIO hand'
                       "/repeat" -> do
                         httpLBS $ keyboard mes
-                        testIO (up_id + 1) dict'
+                        testIO hand'
                       _ -> do
                         let rep_ = read $ fromJust $ (M.lookup u dict') :: Int
                         let rep = Prelude.take rep_ (Prelude.repeat (echoMessage mes)) 
-                        forM_ rep httpLBS -- $ echoMessage mes
+                        mapM_ httpLBS rep
                         print (up_id + 1)
-                        testIO (up_id + 1) dict'
+                        testIO hand'
 
