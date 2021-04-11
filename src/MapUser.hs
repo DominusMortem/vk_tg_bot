b04aeeac0e72be76a1a2461444db5362a7e56877
DominusMortem <80106081+DominusMortem@users.noreply.github.com>
2021-04-11 14:43:47 +0300

    Update MapUser.hs

--- src/MapUser.hs
+++ src/MapUser.hs
@@ -1,24 +1,10 @@
 module MapUser where
 
-import Control.Monad.State (State, get, modify, put, evalState, execState)
-import qualified Data.Map as M
+import Control.Monad.State (State, get, put)
+import qualified Data.Map  as M
 import Data.Text 
 
-testData :: M.Map Int String
-testData = M.empty
-
-mapState :: (Int, String) -> State (M.Map Int String) ()
-mapState (k, v) = do
-  map <- get
-  if (M.lookup k map) == Nothing
-  then put $ M.insert k v map
-  else put $ map
-
-testMap a b = execState $ mapState (a, b)
-
-mapState' :: (Int, String) -> State (M.Map Int String) ()
-mapState' (k, v) = do
+mapSt :: (Int, String) -> State (M.Map Int String) ()
+mapSt (k, v) = do
     dict <- get
     put (M.insert k v dict)
-
-testMap' a b = execState $ mapState' (a, b)
