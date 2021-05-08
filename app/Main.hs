573afc8c295079887a8f9acf0dfd47602753f2cc
DominusMortem <80106081+DominusMortem@users.noreply.github.com>
2021-05-08 11:50:49 +0300

    Add files via upload

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,13 @@
 module Main where
 
-import Network.HTTP.Simple
 import Telegram
-import GHC.Generics
-import Data.Maybe
-import Data.Functor
-import Control.Applicative
-import Control.Monad
-import Data.ByteString.Lazy as B
-import Data.ByteString.Char8 as BS
-import Data.Aeson
-import Data.Text 
-import Data.Text.Encoding (encodeUtf8)
-import Data.Text.IO as TIO
-import qualified Data.ByteString.UTF8 as BU
-import qualified Data.Map as M
+import VK
+import Config
 
-
-main = print "Hello World"
+main :: IO ()
+main = do
+    config <- getConfig
+    case (api config) of
+        "tg" -> startTG config newVarT
+        "vk" -> startVK config newVar
+        _    -> putStrLn "Close application."
